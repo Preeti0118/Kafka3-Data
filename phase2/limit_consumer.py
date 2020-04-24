142130d78a427116403ab2f9dc7a8e0cc7195927
Preeti Sehgal <preetisehgal2001@gmail.com>
2020-04-23 22:01:29 -0400

    printed out balances of all the customers below threshold

---
+++ phase2/limit_consumer.py
@@ -0,0 +1,46 @@
+from kafka import KafkaConsumer, TopicPartition
+from json import loads
+import sqlalchemy
+
+
+class XactionConsumer:
+    def __init__(self):
+        self.consumer = KafkaConsumer('bank-customer-events',
+            bootstrap_servers=['localhost:9092'],
+            # auto_offset_reset='earliest',
+            value_deserializer=lambda m: loads(m.decode('ascii')))
+        ## These are two python dictionarys
+        # Ledger is the one where all the transaction get posted
+        self.ledger = {}
+        # custBalances is the one where the current blance of each customer
+        # account is kept.
+        self.custBalances = {}
+        # THE PROBLEM is every time we re-run the Consumer, ALL our customer
+        # data gets lost!
+        self.limit = -5000
+
+    def handleMessages(self):
+        for message in self.consumer:
+            message = message.value
+            print('{} received'.format(message))
+            self.ledger[message['custid']] = message
+
+            print('ledger= ',self.ledger)
+            # add message to the transaction table in your SQL usinf SQLalchemy
+            if message['custid'] not in self.custBalances:
+                self.custBalances[message['custid']] = 0
+            if message['type'] == 'dep':
+                self.custBalances[message['custid']] += message['amt']
+            else:
+                self.custBalances[message['custid']] -= message['amt']
+
+            print('Balance of the following customers are below threshold- ')
+            for key, value  in self.custBalances.items():
+                 if value < self.limit:
+                    print(key, value)
+
+
+
+if __name__ == "__main__":
+    c = XactionConsumer()
+    c.handleMessages()
